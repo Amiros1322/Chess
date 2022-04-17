c46da9c3bbcac969b1693db6f4a9603141e8c416
Amiros1322 <zehaviamir9@gmail.com>
2022-04-17 11:58:24 +0300

    Playing with surfaces

--- PyGameGUI.py
+++ PyGameGUI.py
@@ -1,26 +1,48 @@
 import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+    MOUSEBUTTONDOWN,
+
+)
 
 # init pygame
 pygame.init()
 
-# start window
-screen = pygame.display.set_mode((500, 500))
-
-# settings
+# settings, constants
 pygame.display.set_caption("Chess")
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
+# start window (returns surface)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # run loop
 running = True
 while running:
+
+    # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
 
         # Fill background with white
         screen.fill((255, 255, 255))
 
-        # Draw a blue circle in the center
-        pygame.draw.circle(screen, (0,0,255), (250, 250), 75)
+        # create a surface. Inputs are its length and width
+        surf = pygame.Surface((50, 50))
+        surf.fill((0, 0, 0))
+        rect = surf.get_rect()  # access the surface's underlying rectangle
+
+        # blit (block transfer) of the surface onto the screen. Tuple is draw location
+        screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
         # Flip display
         pygame.display.flip()
